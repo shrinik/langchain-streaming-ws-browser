7475df0f36d859724e462b8713f7dd5cb92160ef
shrinik <24953626+shrinik@users.noreply.github.com>
2024-04-12 08:59:35 +0530

    update ws address

--- js/chat.js
+++ js/chat.js
@@ -1,16 +1,22 @@
-const ws = new WebSocket('ws://localhost:3000');
+// const ws = new WebSocket('ws://localhost:3000');
+const ws = new WebSocket('wss://4ndsaoio79.execute-api.us-east-1.amazonaws.com/production/');
 
 ws.onopen = function () {
     document.querySelector('#send').addEventListener('click', function (e) {
         e.preventDefault()
         const message = document.querySelector('#message').value;
         document.querySelector('#chatbox').value += "\n\n" + message + "\n"
-        ws.send(message);
+        ws.send(JSON.stringify({action:"sendMessage", Query: message}));
     });
 };
 
 ws.onmessage = function (msg) {
-    document.querySelector('#chatbox').value += msg.data
+    console.log(msg)
+    let data = JSON.parse(msg.data)
+    let responseMessage = data.data
+    if(data.data) {
+        document.querySelector('#chatbox').value += responseMessage
+    }
 };
 
 document.querySelector('#clear').addEventListener('click', function (e) {
